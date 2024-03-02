1e72850141d8d2ad4c57265d57f4a37d144133d9
Your Name <you@example.com>
2024-03-02 13:00:03 +0530

    First commit

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# For sqlite
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+
+# For postgresSQL
+# POSTGRESQL_DATABASE_URL = 'postgresql://postgres:Ss9079581918@localhost/TodoApplicationDatabase'
+# engine = create_engine(POSTGRESQL_DATABASE_URL)
+
+# For Mysql
+MYSQL_DATABASE_URL = 'mysql+pymysql://root:Ss9079581918@127.0.0.1:3306/TodoApplicationDatabase'
+engine = create_engine(MYSQL_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
